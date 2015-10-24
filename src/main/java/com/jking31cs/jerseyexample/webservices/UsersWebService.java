9c6ff3d7764142712cb45bef68b5bb49c9ff58e5
Enrique Saurez <quique7991@gmail.com>
2015-10-24 11:45:57 -0400

    Homework solution

--- src/main/java/com/jking31cs/jerseyexample/webservices/UsersWebService.java
+++ src/main/java/com/jking31cs/jerseyexample/webservices/UsersWebService.java
@@ -47,6 +47,7 @@ public class UsersWebService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public User saveUser(User user) {
+    	System.out.println(user.getName());
         return store.save(user);
     }
 
